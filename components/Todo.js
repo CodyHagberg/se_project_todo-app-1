e9996be1281a25380a89161255ac165d861b987a
CodyHagberg <codyhagberg1117@gmail.com>
2025-10-21 14:37:17 -0700

    Project 7 Part 5

--- components/Todo.js
+++ components/Todo.js
@@ -23,19 +23,33 @@ class Todo {
 
     }
 
+    _dateToString() {
+        this._todoDate = this._todoElement.querySelector(".todo__date");
+        const dueDate = new Date(this._data.date);
+        if (!isNaN(dueDate)) {
+          this._todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}`;
+        }
+    }
+
     getView() {
         this._todoElement = this._templateElement.content
     .querySelector(".todo")
    .cloneNode(true);
   const todoNameEl = this._todoElement.querySelector(".todo__name");
-  const todoDate = this._todoElement.querySelector(".todo__date");
-  const todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+  this._todoDate = this._todoElement.querySelector(".todo__date");
+  this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
     todoNameEl.textContent = this._data.name;
 
     this._generateCheckboxEl();
     this._setEventListeners();
+    this._dateToString();
 
   return this._todoElement;
     }
 }
+
 export default Todo;
